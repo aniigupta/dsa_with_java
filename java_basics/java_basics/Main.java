23bffc4c3fce807dfb6c983ae90bf53d575cc829
aniigupta <aniket8076206264@gmail.com>
2023-06-09 19:39:49 +0530

    g

--- java_basics/java_basics/Main.java
+++ java_basics/java_basics/Main.java
@@ -1,6 +1,8 @@
-public class Main{
+package java_basics;
+public Class Main{
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
     }
-};
+}
+
